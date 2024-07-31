bf456ef61730d3eb86fa2067e4033617858328dc
WaseemUddinSecA <n01583077@humbermail.ca>
2024-07-31 19:29:34 -0400

    upgrade
    upgraded all of my files and changed the background image and font

--- home.js
+++ home.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
         navLinks.classList.toggle("show");
     });  
 });
+const banner = document.getElementById('animated-banner');
+    if (banner) {
+        banner.classList.add('animate');
+    }
+
+    const heroSection = document.getElementById('hero');
+    if (heroSection) {
+        // Use setTimeout to delay the addition of the 'show' class
+        setTimeout(() => {
+            heroSection.classList.add('show');
+        }, 100); // Delay in milliseconds (100ms for a quick delay)
+    }
+;
