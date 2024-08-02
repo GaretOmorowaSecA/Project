839e412109b27c0121b1ea73a557d9f6b8eb6ada
victoremmanuelsecA <n01694860@humber.ca>
2024-08-02 13:03:14 -0400

    still working

--- franchising.js
+++ franchising.js
@@ -1,18 +1,21 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Form submitted!');
-});
-
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
     let slides = document.querySelectorAll(".mySlides");
+    let animations = ['slide-up', 'slide-left', 'slide-right'];
+
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  // Hide all slides
+        slides[i].classList.remove(...animations);  // Remove previous animation classes
     }
+    
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}  // Reset index if it exceeds the number of slides
-    slides[slideIndex-1].style.display = "block";  // Show the current slide
+
+    let currentSlide = slides[slideIndex - 1];
+    currentSlide.style.display = "block";  // Show the current slide
+    currentSlide.classList.add(animations[slideIndex - 1]);  // Add animation class
+
     setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
